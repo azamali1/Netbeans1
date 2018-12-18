8c2918b4d252d1a00a0bfaa4717417afb4a9b42a
azamali <azamali@btslivh.eu>
2018-12-18 14:49:18 +0100

    La suite de ma vie

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: snir2g2
+ *
+ * Created on 18 décembre 2018, 14:45
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    cout << "Le nouveau est revenu et il m'en veut pas pour ses côtes !" << endl;
+
+    return 0;
+}
+
