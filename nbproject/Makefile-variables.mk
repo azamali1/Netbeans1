e57dfee8b189aedcb94f45bde54661094cda2b3d
dnovier <dnovier@btslivh.eu>
2018-12-20 14:25:01 +0100

    salut

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=GNU-Linux
 CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
-CND_ARTIFACT_NAME_Debug=tp04
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/tp04
+CND_ARTIFACT_NAME_Debug=netbeans1
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/netbeans1
 CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
-CND_PACKAGE_NAME_Debug=tp04.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/tp04.tar
+CND_PACKAGE_NAME_Debug=netbeans1.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/netbeans1.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
-CND_ARTIFACT_NAME_Release=tp04
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/tp04
+CND_ARTIFACT_NAME_Release=netbeans1
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/netbeans1
 CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
-CND_PACKAGE_NAME_Release=tp04.tar
-CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/tp04.tar
+CND_PACKAGE_NAME_Release=netbeans1.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/netbeans1.tar
 #
 # include compiler specific variables
 #
